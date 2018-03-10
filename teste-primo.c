6e4b919b0770c19a83c5fc010e3ec44d89f9d9f6
guilherme1guy <guilhermeguy349@gmail.com>
2018-03-10 08:32:52 -0300

    solution to prime number test

--- teste-primo.c
+++ teste-primo.c
@@ -1,10 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-bool primo(long long n) {
+int primo(long long n) {
 	//Seu código
+
+	int resultado = 1;
+
+	for (int i = n-1; i > 1; --i) {
+		
+		if(n % i == 0){
+			resultado = 0;
+			break;
+		}
+
+	}
+
+	return resultado;
+
 }
 
 int main() {
 	//Seu código
+
+	int num = 0;
+
+	printf("Digite um numero: ");
+	scanf("%d", &num);
+
+	int resultado = primo(num);
+
+	if(resultado == 0){
+			printf("Nao primo");
+	}else{
+		printf("Primo");
+	}
+
+
 }
