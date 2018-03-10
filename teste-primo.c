34e0c38ed3933faa3b4964dbb6a7e359b76fcf08
GeovanaRamos <geovannna.1998@gmail.com>
2018-03-10 08:47:54 -0300

    Finding prime numbers

--- teste-primo.c
+++ teste-primo.c
@@ -1,10 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-bool primo(long long n) {
+int primo(long long n) {
 	//Seu código
+  long long i = 2;
+
+  while(i < n){
+    if (n%i == 0){
+      return 1;
+      break;
+    }
+    i++;
+  }
+  return 0;
 }
 
 int main() {
 	//Seu código
+  long long number;
+  int answer;
+
+  scanf("%lld", &number );
+
+  answer = primo(number);
+
+  if (answer==1){
+    printf("Não é primo\n");
+  }else{
+    printf("É primo\n");
+  }
+
 }
