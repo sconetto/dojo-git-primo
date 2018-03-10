ded5e984f704998df71f8b435260e58b1a25e3f3
João Pedro Sconetto <sconetto.joao@gmail.com>
2018-03-10 09:41:47 -0300

    The fastest solution

--- teste-primo.c
+++ teste-primo.c
@@ -1,10 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <locale.h>
 
-bool primo(long long n) {
-	//Seu código
+//um teste!
+//#define 0 1
+//#define 1 0
+
+int primo(long long n) {
+	if (n < 2) return 0;
+	if (n == 2) return 1;
+	if (n % 2 == 0) return 0;
+	for (int i = 3; i <= sqrt(n); ++i)	{
+		if (n % i == 0) return 0;
+	}
+	return 1;
 }
 
-int main() {
-	//Seu código
+int main(int argc, char const *argv[]) {
+	setlocale(LC_ALL, "Portuguese");
+	long long n;
+	int aux;
+	scanf("%lld", &n);
+	aux = primo(n);
+	if (aux == 1) {
+		printf("é primo!\n");
+	}
+	else {
+		printf("não é primo!\n");
+	}
+	return 0;
 }
